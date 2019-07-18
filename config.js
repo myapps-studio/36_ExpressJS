3812907ca9242da08c554e9f25ac5e860212a4e3
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-07-18 14:29:57 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '764830134152-527km73r1smg6lgjje5736uo1ldsrdkg.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'N_5KPxTYAGH6JmtpOshIs-74',
+    'CALLBACK_URL': 'http://localhost/auth/google/callback'
+};
